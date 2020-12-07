315914dc927c30e8fea106a5ffabf6041c56992c
tlanclos15 <tlanclos@uncc.edu>
2020-12-07 18:16:27 -0500

    first commit for final project

---
+++ Final Project/server/server.js
@@ -0,0 +1,54 @@
+//Budget API
+
+//const express = require('express');
+const cors = require('cors');
+//const app = express();
+const port = 8080;
+
+//app.use(express.static('/public'));
+app.use(cors());
+
+/*const budget = {
+    myBudget: [
+    {
+        title: 'Eat out',
+        budget: 25
+    },
+    {
+        title: 'Rent',
+        budget: 375   
+    },
+    {
+        title: 'Grocery',
+        budget: 110   
+    },
+    {
+        title: 'Monthly Investment',
+        budget: 25
+    },
+    {
+        title: 'Gas',
+        budget: 40   
+    },
+    {
+        title: 'Monthly Savings Investment',
+        budget: 110   
+    },
+    {
+        title: 'Monthly Student Loans',
+        budget: 250   
+    },
+]
+};
+
+//app.get('/hello', (req, res) => {
+//    res.send('Hello World!');
+//});
+
+app.get('/budget', (req, res) => {
+    res.json(budget);
+});
+
+app.listen(port, () => {
+    console.log(`API served at http://localhost:${port}`);
+}); */
